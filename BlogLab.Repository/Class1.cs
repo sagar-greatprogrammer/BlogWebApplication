524c6492f0f5b28fb6e7d131a0e63f0b16514e11
sagar-greatprogrammer <shatagale123@gmail.com>
2023-01-22 04:32:19 +0530

    Models project complete. Repository project created with Account Repository.

---
+++ BlogLab.Repository/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogLab.Repository
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ BlogLab.Repository/IAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogLab.Models.Account;
+using Microsoft.AspNet.Identity;
+
+namespace BlogLab.Repository
+{
+    public interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user,
+            CancellationToken cancellationToken);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername,
+            CancellationToken cancellationToken);
+    }
+}
